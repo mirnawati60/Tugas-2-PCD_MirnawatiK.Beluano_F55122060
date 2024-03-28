43ab5d90bef67ae3b525784d98da80d3ad129308
Asus VivoBook <mirnabeluano805@gmail.com>
2024-03-28 23:17:29 +0800

    first init

---
+++ TranformasiNegatif.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread("mammogram.jpg", 0)
+
+img_1 = 255 - img
+
+cv2.imshow("Original image", img)
+cv2.imshow("Image Negative", img_1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
